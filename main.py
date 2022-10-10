5666564ded0bfc5554e252d22a26a943cea8cd53
guessing-game <linaaaah420@gmail.com>
2022-10-10 10:12:52 +0100

    Finished Guessing game

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+secret_number = 9
+guessing_count = 0
+guess_limit = 3
+while guessing_count < guess_limit:
+    guess = int(input("Guess: "))
+    guessing_count += 1
+    if guess == secret_number:
+        print ("Well done! ")
+        break
+else:
+    print("You failed! ")
+
+
 
